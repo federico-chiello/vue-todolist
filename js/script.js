df47eff9d1c3d970599be0c0026526890e9842da
Fede <chiellofederico@gmail.com>
2021-01-28 15:11:23 +0100

    Init css

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Realizzare una todolist con Vue. La lista deve essere già popolata con alcuni elementi e si deve dare la possibilità di aggiungere nuovi item e di cancellarli.
+// Bonus 1): permettere ai file cancellati di finire in un'area 'cestino' dove possono essere eliminati del tutto oppure ripristinati.
+// Bonus 2): Dare la possibilità di eliminare tutti i file dal cestino
+// Aggiungete quello che più vi piace a piacere!
